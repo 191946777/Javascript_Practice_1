f01def8106a841aaa492348156632ab01011b497
Aman Singh <amansingh36645@gmail.com>
2026-03-22 16:07:36 +0530

    Set 1

---
+++ script.js
@@ -0,0 +1,21 @@
+let obj = {
+  name: "aman",
+  phn_number: 7054764217,
+  marks: 97,
+};
+
+obj.age = 65;
+
+console.log(obj);
+
+let str = "65";
+
+console.log(typeof str);
+
+const obj1 = {
+  name: "aman",
+};
+
+obj1.num = 65;
+
+console.log(obj1);
