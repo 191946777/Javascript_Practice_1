2356bc831b74d8b114941146cc2340ded47ede35
Aman Singh <32776959+191946777@users.noreply.github.com>
2026-06-28 16:27:56 +0530

    Chapter 3
    Completed all code of Chapter 3 JS

--- script.js
+++ script.js
@@ -74,3 +74,68 @@ let age = 17;
 let result = age >= 18 ? "can drive" : "not drive";
 
 console.log(result);
+
+// CHAPTER 3
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+for (const key in obj) {
+    console.log(key);
+}
+
+let arr = ["aman","singh","sweety","sharma"]
+
+for (const ele of arr) {
+    console.log(ele);
+
+}
+let i = 1;
+do {
+    console.log(i);
+    i++
+
+} while ( i < 5)
+
+let obj = { aman: 99, sweety: 97, sonali: 95 };
+
+for (const key in obj) {
+  console.log(obj[key]);
+}
+
+let key = Object.keys(obj);
+console.log(key);
+
+for (let i = 0; i < key.length; i++) {
+  console.log(obj[key[i]]);
+}
+
+function guessNum(para1){
+    if(para1 == 55){
+        console.log("Number Guessed");
+
+    } else {
+        console.log("try again");
+
+    }
+
+}
+
+guessNum(prompt("enter the number"));
+
+
+let arr = [10, 20, 30, 40, 50];
+
+function mean() {
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+
+  }
+  let avg = result / arr.length
+  console.log(avg);
+}
+
+mean();
